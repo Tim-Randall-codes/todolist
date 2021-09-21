3b4dbffa1d8f5be90a016f60f39d1a6b697ccaba
Tim Randall <trtimrandall@gmail.com>
2021-09-21 22:43:20 +1000

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Tim Randall on 21/9/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
